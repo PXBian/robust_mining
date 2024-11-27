11385c94b0234b0aff821a4ed4b90a2acc5b7071
Pengxin Bian <1053189561@qq.com>
2024-11-27 10:02:31 +0000

    Add casestudy with SARS dataset-v1

--- casestudy_sars.sh
+++ casestudy_sars.sh
@@ -6,6 +6,12 @@ mkdir output
 mkdir peak_ram
 
 (
-    (/usr/bin/time -v ./case /scratch_tmp/prj/proj_loukides/ling/kkp/examples/sars_cleaned/sars_0-1000K_top1.fna 5000 5      data/sars_1.txt)   &> peak_ram/casestudy_sars_500_5.txt     &
-    (/usr/bin/time -v ./case /scratch_tmp/prj/proj_loukides/ling/kkp/examples/sars_cleaned/sars_0-1000K_top1.fna 10000 5      data/sars_1.txt)   &> peak_ram/casestudy_sars_500_5.txt     &
+    (/usr/bin/time -v ./case /scratch_tmp/prj/proj_loukides/ling/kkp/examples/sars_cleaned/sars_0-1000K_top1.fna 500   5    data/sars_1.txt)   &> peak_ram/casestudy_sars_500_5.txt     &
+    (/usr/bin/time -v ./case /scratch_tmp/prj/proj_loukides/ling/kkp/examples/sars_cleaned/sars_0-1000K_top1.fna 1000  5    data/sars_1.txt)   &> peak_ram/casestudy_sars_1000_5.txt    &
+    (/usr/bin/time -v ./case /scratch_tmp/prj/proj_loukides/ling/kkp/examples/sars_cleaned/sars_0-1000K_top1.fna 10000 5    data/sars_1.txt)   &> peak_ram/casestudy_sars_10000_5.txt   &
+
+    (/usr/bin/time -v ./case /scratch_tmp/prj/proj_loukides/ling/kkp/examples/sars_cleaned/sars_0-1000K_top1.fna 500   50    data/sars_1.txt)   &> peak_ram/casestudy_sars_500_50.txt     &
+    (/usr/bin/time -v ./case /scratch_tmp/prj/proj_loukides/ling/kkp/examples/sars_cleaned/sars_0-1000K_top1.fna 1000  50    data/sars_1.txt)   &> peak_ram/casestudy_sars_1000_50.txt    &
+    (/usr/bin/time -v ./case /scratch_tmp/prj/proj_loukides/ling/kkp/examples/sars_cleaned/sars_0-1000K_top1.fna 10000 50    data/sars_1.txt)   &> peak_ram/casestudy_sars_10000_50.txt   &
+
 )
